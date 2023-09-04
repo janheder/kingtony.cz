f59824cdf27eedb3ea57e09a488047ea14516eb9
Jan Heder <info@janheder.cz>
2023-09-04 10:21:51 +0200

    .

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,27 +1,3 @@
-// -----------------------------------------------------------------------------
-// COOKIEBAR
-// -----------------------------------------------------------------------------
-
-
-var cookiebar = new Cookiebar({
-    id: "cookiebar",
-    cls: "cookiebar",
-    cookie: "cookiebar",
-    content: {
-        description: "Tato stránka používá cookies za účelem optimalizace efektivního poskytování služeb.",
-        link: "Více informací",
-        href: "http://ec.europa.eu/ipg/basics/legal/cookies/index_en.htm",
-        button: "Rozumím",
-        more: "..."
-    },
-    fade: {
-        type: "in",
-        ms: "500",
-        display: "inline"
-    },
-    debug: 0
-});
-
 
 // -----------------------------------------------------------------------------
 // LAZYLAOD INIT
