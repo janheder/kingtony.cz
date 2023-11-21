18ebb878e00b960ea473416598d2bf62cdac8618
Jan Heder <info@janheder.cz>
2023-11-21 22:17:58 +0100

    .

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -303,6 +303,20 @@ for (let loop = 0; loop < navitem.length; loop++) {
 }
 
 
+$(".nav-linkDesktop").hover(
+    function () {
+      setTimeout(function(){
+        $(this).closest(".nav-item.expandable").addClass("result_hover");
+    }.bind(this), 600);
+  
+    },
+    function () {
+      setTimeout(function(){
+        $(this).closest(".nav-item.expandable").removeClass("result_hover");
+    }.bind(this), 600);
+    }
+  );
+
 // -----------------------------------------------------------------------------
 // PRODUCT DETAIL VARIANTS
 // -----------------------------------------------------------------------------
